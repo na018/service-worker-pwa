664d8c31bc466cfac9370fde1480d08e84cfdb87
Haag Immanuel <ih038@hdm-stuttgart.de>
2018-06-03 22:52:25 +0200

    added new menu feature

--- start.sh
+++ start.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
-
 . .env
-docker-compose build --no-cache
-docker-compose up 
+PS3='Please enter your choice or ctrl-c for quit: '
+options=("startserver" "generatekeys" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "startserver")
+            docker-compose build --no-cache
+            docker-compose up
+            ;;
+        "generatekeys")
+            echo "running in container npm for vapid keys" && docker exec -ti sw_web_1 ash -c "npm run web-push generate-vapid-keys"
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo invalid option;;
+    esac
+done
+
+
+#docker-compose build --no-cache
+#docker-compose up 
 #fixme - i need a new menu for that stuff!
 #if [ "$1" == "addvapidkeys" ]; then
  # echo "running in container npm for vapid keys" && docker exec -ti sw_web_1 ash -c "npm run web-push generate-vapid-keys"
