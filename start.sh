d21e0e638a69af9fbe2b39be3cc3f45aff0a74c2
Haag Immanuel <ih038@hdm-stuttgart.de>
2018-06-05 19:40:37 +0200

    adjust start script for keygeneration and added gitignore

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . .env
-PS3='Please enter your choice or ctrl-c for quit: '
+PS3='Please enter your choice or ctrl-c for quit: 1) startserver 2) generatekeys 3)Quit '
 options=("startserver" "generatekeys" "Quit")
 select opt in "${options[@]}"
 do
@@ -10,7 +10,7 @@ do
             docker-compose up
             ;;
         "generatekeys")
-            echo "running in container npm for vapid keys" && docker exec -ti sw_web_1 ash -c "npm run web-push generate-vapid-keys"
+            echo "running in container npm for vapid keys - if you change them you have to adjust the app.js public Key!" && docker exec -ti sw_web_1 ash -c "npm run web-push generate-vapid-keys >> keys.txt"
             ;;
         "Quit")
             break
@@ -21,9 +21,7 @@ done
 
 
 #docker-compose build --no-cache
-#docker-compose up 
+#docker-compose up
 #fixme - i need a new menu for that stuff!
 #if [ "$1" == "addvapidkeys" ]; then
  # echo "running in container npm for vapid keys" && docker exec -ti sw_web_1 ash -c "npm run web-push generate-vapid-keys"
-
-
