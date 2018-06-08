bd4322d5213d8e305c3c25210687895f18ff3e08
Haag Immanuel <ih038@hdm-stuttgart.de>
2018-06-08 18:43:32 +0200

    enabled pushnotifcation

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,4 +1,119 @@
-var deferredPrompt
+
+var deferredPrompt;
+var enableNotificationsButtons = document.querySelectorAll('.enable-notifications');
+
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
+
+function displayConfirmNotification() {
+  if ('serviceWorker' in navigator) {
+    var options = {
+      body: 'You successfully subscribed to our Notification service!',
+      icon: '/src/images/icons/app-icon-96x96.png',
+      image: '/src/images/sf-boat.jpg',
+      dir: 'ltr',
+      lang: 'en-US', // BCP 47,
+      vibrate: [100, 50, 200],
+      badge: '/src/images/icons/app-icon-96x96.png',
+      tag: 'confirm-notification',
+      renotify: true,
+      actions: [
+        { action: 'confirm', title: 'Okay', icon: '/src/images/icons/app-icon-96x96.png' },
+        { action: 'cancel', title: 'Cancel', icon: '/src/images/icons/app-icon-96x96.png' }
+      ]
+    };
+
+    navigator.serviceWorker.ready
+      .then(function(swreg) {
+        swreg.showNotification('Successfully subscribed!', options);
+      });
+  }
+}
+
+function configurePushSub() {
+  if (!('serviceWorker' in navigator)) {
+    return;
+  }
+
+  var reg;
+  navigator.serviceWorker.ready
+    .then(function(swreg) {
+      reg = swreg;
+      return swreg.pushManager.getSubscription();
+    })
+    .then(function(sub) {
+      if (sub === null) {
+        // Create a new subscription
+        var vapidPublicKey = 'BDui3XmS-VxBFTLMxLK3GPgW0BIdJSVlvXQR-6v--kHisyNz2Os2hOZUVPIyrljgQ7CbZBAsNov1oUX9AsYVOLw';
+        var convertedVapidPublicKey = urlBase64ToUint8Array(vapidPublicKey);
+        return reg.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: convertedVapidPublicKey
+        });
+      } else {
+        // We have a subscription
+      }
+    })
+    .then(function(newSub) {
+      return fetch('https://serviceworker-f7f4f.firebaseio.com/subscription.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify(newSub)
+      })
+    })
+    .then(function(res) {
+      if (res.ok) {
+        displayConfirmNotification();
+      }
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+function askForNotificationPermission() {
+  Notification.requestPermission(function(result) {
+    console.log('User Choice', result);
+    if (result !== 'granted') {
+      console.log('No notification permission granted!');
+    } else {
+      configurePushSub();
+      // displayConfirmNotification();
+    }
+  });
+}
+
+if ('Notification' in window && 'serviceWorker' in navigator) {
+  for (var i = 0; i < enableNotificationsButtons.length; i++) {
+    enableNotificationsButtons[i].style.display = 'inline-block';
+    enableNotificationsButtons[i].addEventListener('click', askForNotificationPermission);
+  }
+}
+
+
+/*var deferredPrompt
 var enableNotiButton = document.querySelectorAll('.enable-notifications');
 
 // if browser has not implemented Promise polyfill
@@ -99,10 +214,12 @@ function configurePushSub() {
         });
       } else {
         // subscription already there!
+        console.log("no sub needed");
       }
     })
      .then(function(newSub) {
-       return fetch('https://serviceworker-f7f4f.firebaseio.com/subscriptions.json', {
+       console.log("before POST");
+       return fetch('https://serviceworker-f7f4f.firebaseio.com/subscription.json', {
          method: 'POST',
          headers: {
            'Content-Type': 'application/json',
